f0a8d381bf859b55bf45baa97e48a34a790e427a
Asciotti <andrew.sciotti@gmail.com>
2018-10-02 09:13:25 -0400

    handled remainder cols when not even split
    also cleaned up some portions and now it saves files
    stil looking to make executable and UIgetfile portion for inputs

--- split_excel_sheet.py
+++ split_excel_sheet.py
@@ -1,16 +1,18 @@
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+from openpyxl import Workbook
 import argparse
 
 #Copy range of cells as a nested list
 #Takes: start cell, end cell, and sheet you want to copy from.
+#src: https://yagisanatode.com/2017/11/18/copy-and-paste-ranges-in-excel-with-openpyxl-and-python-3/
 def copyRange(startCol, startRow, endCol, endRow, sheet):
     rangeSelected = []
     #Loops through selected Rows
-    for i in range(startRow,endRow + 1,1):
+    for i in range(startRow,endRow + 1):
         #Appends the row to a RowSelected list
         rowSelected = []
-        for j in range(startCol,endCol+1,1):
+        for j in range(startCol,endCol+1):
             rowSelected.append(sheet.cell(row = i, column = j).value)
         #Adds the RowSelected List and nests inside the rangeSelected
         rangeSelected.append(rowSelected)
@@ -19,11 +21,12 @@ def copyRange(startCol, startRow, endCol, endRow, sheet):
 
 #Paste range
 #Paste data from copyRange into template sheet
+#src: https://yagisanatode.com/2017/11/18/copy-and-paste-ranges-in-excel-with-openpyxl-and-python-3/
 def pasteRange(startCol, startRow, endCol, endRow, sheetReceiving,copiedData):
     countRow = 0
-    for i in range(startRow,endRow+1,1):
+    for i in range(startRow,endRow+1):
         countCol = 0
-        for j in range(startCol,endCol+1,1):
+        for j in range(startCol,endCol+1):
             
             sheetReceiving.cell(row = i, column = j).value = copiedData[countRow][countCol]
             countCol += 1
@@ -44,18 +47,36 @@ def split_workbook(file, dir_out, split_size):
 
     # Get number of cols each subfile should hold, and remainder
     (num_files, remainder) = divmod(total_cols, split_size)
+
+    if remainder > 0:
+        print('Warning: the last subfile will have {} + {} columns'.format(split_size, remainder))
     
     # Create subfiles containing only split_size number of cols of original
     for subfile in range(num_files):
+        # Create new workbook name
+        dest_filename = '{}_test.xlsx'.format(subfile)
+        # Create new workbook
+        new_file = Workbook()
+        new_sheet = new_file.active
+        # Get data to copy
         if subfile == 0:
-            start_col = get_column_letter(1)
-            end_col = get_column_letter(subfile*split_size+split_size)
+            start_col = 1
+            end_col = subfile*split_size+split_size
+        elif subfile == len(range(num_files))-1:
+            start_col = subfile*split_size+1
+            end_col = subfile*split_size+split_size+remainder
         else:
-            start_col = get_column_letter(subfile*split_size+1)
-            end_col = get_column_letter(subfile*split_size+split_size)
+            start_col = subfile*split_size+1
+            end_col = subfile*split_size+split_size
         # Copy data
-        selected_data =  copyRange(start_col, end_col, 1, 20, sheet)
+        selected_data =  copyRange(start_col, 1, end_col, max_rows, sheet)
         # Paste data
+        if subfile == len(range(num_files))-1:
+            pasteRange(1, 1, split_size+remainder, max_rows, new_sheet, selected_data)
+        else:
+            pasteRange(1, 1, split_size, max_rows, new_sheet, selected_data)
+        print('Filled subfile {}'.format(subfile))
+        new_file.save(filename = dest_filename)
 
 
 if __name__ == '__main__':
