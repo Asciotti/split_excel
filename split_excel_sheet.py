b22caf5b85c9a5ed17109edd561f575f108be7ed
Asciotti <andrew.sciotti@gmail.com>
2018-10-03 08:22:34 -0400

    change split by col to rows
    also added header

--- split_excel_sheet.py
+++ split_excel_sheet.py
@@ -25,10 +25,11 @@ def copyRange(startCol, startRow, endCol, endRow, sheet):
 #src: https://yagisanatode.com/2017/11/18/copy-and-paste-ranges-in-excel-with-openpyxl-and-python-3/
 def pasteRange(startCol, startRow, endCol, endRow, sheetReceiving,copiedData):
     countRow = 0
+    print(copiedData)
     for i in range(startRow,endRow+1):
         countCol = 0
         for j in range(startCol,endCol+1):
-            
+            print(countRow, countCol)
             sheetReceiving.cell(row = i, column = j).value = copiedData[countRow][countCol]
             countCol += 1
         countRow += 1
@@ -44,16 +45,16 @@ def split_workbook(file, dir_out, split_size):
     sheet = wb.worksheets[0]
 
     # Get num of cols
-    total_cols = sheet.max_column
+    max_cols = sheet.max_column-1 # there is a header
 
     # Get max num of rows
-    max_rows = sheet.max_row
+    total_rows = sheet.max_row
 
-    # Get number of cols each subfile should hold, and remainder
-    (num_files, remainder) = divmod(total_cols, split_size)
-
+    # Get number of cols each subfile should hold, and remainder, -1 because of header
+    (num_files, remainder) = divmod(total_rows-1, split_size)
+    print(num_files, remainder)
     if remainder > 0:
-        print('Warning: the last subfile will have {} + {} columns'.format(split_size, remainder))
+        print('Warning: the last subfile will have {} + {} rows'.format(split_size, remainder))
     
     # Create subfiles containing only split_size number of cols of original
     for subfile in range(num_files):
@@ -62,23 +63,27 @@ def split_workbook(file, dir_out, split_size):
         # Create new workbook
         new_file = Workbook()
         new_sheet = new_file.active
-        # Get data to copy
+        # Get data to copy, after first everything has +1 row due to header
         if subfile == 0:
-            start_col = 1
-            end_col = subfile*split_size+split_size
-        elif subfile == len(range(num_files))-1:
-            start_col = subfile*split_size+1
-            end_col = subfile*split_size+split_size+remainder
+            start_row = 2
+            end_row = subfile*split_size+split_size+1
+        elif subfile == num_files-1:
+            start_row = subfile*split_size+2
+            end_row = subfile*split_size+split_size+remainder+1
         else:
-            start_col = subfile*split_size+1
-            end_col = subfile*split_size+split_size
+            start_row = subfile*split_size+2
+            end_row = subfile*split_size+split_size+1
+        # Copy header
+        header = copyRange(1, 1, max_cols, 1, sheet)
+        # Paste header
+        pasteRange(1, 1, max_cols, 1, new_sheet, header)
         # Copy data
-        selected_data =  copyRange(start_col, 1, end_col, max_rows, sheet)
+        data =  copyRange(1, start_row, max_cols, end_row, sheet)
         # Paste data
-        if subfile == len(range(num_files))-1:
-            pasteRange(1, 1, split_size+remainder, max_rows, new_sheet, selected_data)
+        if subfile == num_files-1:
+            pasteRange(1, 2, max_cols, split_size+remainder+1, new_sheet, data)
         else:
-            pasteRange(1, 1, split_size, max_rows, new_sheet, selected_data)
+            pasteRange(1, 2, max_cols, split_size+1, new_sheet, data)
         print('Filled subfile {}'.format(subfile))
         new_file.save(filename = dest_filename)
 
@@ -88,7 +93,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Splits 1 excel file into X smaller files, as equally as possible based on # of columns')
     parser.add_argument('file', help='Excel file to split')
     parser.add_argument('dir_out', help='Directory to save split files to')
-    parser.add_argument('split_size', help='Number of columns to split by')
+    parser.add_argument('split_size', help='Number of rows to split by')
 
     # Get arguments
     args = parser.parse_args()
