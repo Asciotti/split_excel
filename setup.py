18f0f7064c65c4b4c48e3b970726a1c79d147ef1
Asciotti <andrew.sciotti@gmail.com>
2018-10-05 22:28:32 -0400

    setup.py first try

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="split_excel",
+    version="0.0.1",
+    author="Andrew Sciotti",
+    author_email="andrew.sciotti@gmail.com",
+    description="A simple script to split an excel sheet by rows",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Asciotti/split_excel",
+    packages=['src'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha"
+    ],
+)
